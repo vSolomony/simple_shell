5004c4d3432c495fffca5569b003b1fe004de00a
vSolomony <mohamed.a.kassem95@gmail.com>
2023-09-20 05:35:28 -0700

    adj

--- main.c
+++ main.c
@@ -1,25 +1,42 @@
 #include "shell.h"
 /**
  * main - entry level
+ * @argc: nuumber of args
+ * @argv: number of the char of each arg
  * Return: 0 (Sucess)
 */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char the_command[128];
+	int i;
 
-	while (1)
+	if (argc > 1)
 	{
-		to_print("Solomony_Shell$ ");
-		if (to_read_command(the_command, sizeof(the_command)) == 0)
+		for (i = 1; i < argc; i++)
 		{
-			to_print("\n");
-			break;
-		}
-		if (to_exec_command(the_command) == -1)
-		{
-			to_print("there's no command!!");
+			if (to_exec_command(argv[i]) == -1)
+			{
+				to_print("there's no such command");
+			}
 		}
 	}
+	else
+	{
 
+		char the_command[128];
+
+		while (1)
+		{
+			to_print("Solomony_Shell$ ");
+			if (to_read_command(the_command, sizeof(the_command)) == 0)
+			{
+				to_print("\n");
+				break;
+			}
+			if (to_exec_command(the_command) == -1)
+			{
+				to_print("there's no command!!");
+			}
+		}
+	}
 	return (0);
 }
