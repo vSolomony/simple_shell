d2f8a38caf50206f5d1f03053dd56e4637811c68
vSolomony <mohamed.a.kassem95@gmail.com>
2023-09-20 09:50:34 -0700

    adj

--- to_exec_command.c
+++ to_exec_command.c
@@ -6,28 +6,28 @@
 */
 void to_exec_command(char *args[])
 {
-    pid_t pid = fork();
-    int status;
+	pid_t pid = fork();
+	int status;
 
-    if (pid < 0)
-    {
-        perror("fork has failed");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (execve(args[0], args, environ) == -1)
-        {
-            perror("execve has failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        if (waitpid(pid, &status, 0) == -1)
-        {
-            perror("child has not arrived");
-            exit(EXIT_FAILURE);
-        }
-    }
+	if (pid < 0)
+	{
+		perror("fork has failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror("execve has failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("child has not arrived");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
