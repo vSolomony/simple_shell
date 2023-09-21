16ef305667cdcfd39798727b2740043b6156cbeb
vSolomony <mohamed.a.kassem95@gmail.com>
2023-09-21 02:11:53 -0700

    handle exit

--- interactive_shellmode.c
+++ interactive_shellmode.c
@@ -11,20 +11,29 @@ void interactive_SHELLMODE(void)
 	{
 		to_print("Solomony_Shell$ ");
 		line = to_read_command();
+
 		if (line != NULL)
 		{
 			if (_strcmp(line, "exit") == 0)
 			{
+				free(line);
 				handle_exit();
 			}
 			else if (_strcmp(line, "env") == 0)
 			{
+				free(line);
 				handle_env();
 			}
 			else
 			{
-			check_input(line);
+				check_input(line);
+				free(line);
 			}
+		}
+		else
+		{
+			to_print("\n");
+			handle_exit();
 		}
 	}
 }
